6bd3f5cf4a4da1c703d49f9bc0686ad4a20684f3
Tre <tregallowayofficial@gmail.com>
2023-07-17 10:47:48 -0500

    Made array for objects to pass in project card

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -10,6 +10,54 @@ import { RiGithubFill, RiArrowRightUpLine, RiPagesLine } from 'react-icons/ri';
 import Skills from '../../components/skills';
 import { Button } from '@/components/ui/button';
 
+const components: { title: string; href: string; description: string; github: string; }[] = [
+	{
+	  title: "Alert Dialog",
+	  href: "/docs/primitives/alert-dialog",
+	  description:
+		"A modal dialog that interrupts the user with important content and expects a response.",
+	  github:
+	},
+	{
+	  title: "Hover Card",
+	  href: "/docs/primitives/hover-card",
+	  description:
+		"For sighted users to preview content available behind a link.",
+	  github:
+	},
+	{
+	  title: "Progress",
+	  href: "/docs/primitives/progress",
+	  description:
+		"Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+		github:
+
+	},
+	{
+	  title: "Scroll-area",
+	  href: "/docs/primitives/scroll-area",
+	  description: "Visually or semantically separates content.",
+	  github:
+
+	},
+	{
+	  title: "Tabs",
+	  href: "/docs/primitives/tabs",
+	  description:
+		"A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+		github:
+
+	},
+	{
+	  title: "Tooltip",
+	  href: "/docs/primitives/tooltip",
+	  description:
+		"A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+		github:
+
+	},
+  ]
+  
 export default function Portfolio() {
 	return (
 		<section>
