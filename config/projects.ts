6051a6151947548c76ac8eeb8edb05cf36a5aeee
Tre <tregallowayofficial@gmail.com>
2023-07-26 18:44:06 -0500

    Updated project config

--- config/projects.ts
+++ config/projects.ts
@@ -2,15 +2,15 @@ import { ProjectItem } from '@/types';
 
 export const projectItem: ProjectItem[] = [
 	{
-		title: 'Alert Dialog',
+		title: 'MyDocs',
 		image:
 			'https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-		stack: '.',
+		stack: 'Nextjs, TailwindCSS, Supabase',
 		href: '/docs/primitives/alert-dialog',
 		description:
-			'A modal dialog that interrupts the user with important content and expects a response.',
+			'A little passion project aimed to take personal notaking and help you use it for current projects. Follow along as we figure this out together.',
 		github: 'github.com',
-		live: 'live.com'
+		live: 'https://my-docs-rosy.vercel.app/'
 	},
 	{
 		title: 'Hover Card',
@@ -19,49 +19,6 @@ export const projectItem: ProjectItem[] = [
 		stack: '.',
 		href: '/docs/primitives/hover-card',
 		description: 'For sighted users to preview content available behind a link.',
-		github: 'github.com',
-		live: 'live.com'
-	},
-	{
-		title: 'Progress',
-		image:
-			'https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-		stack: '.',
-		href: '/docs/primitives/progress',
-		description:
-			'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-		github: 'github.com',
-		live: 'live.com'
-	},
-	{
-		title: 'Scroll-area',
-		image:
-			'https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-		stack: '.',
-		href: '/docs/primitives/scroll-area',
-		description: 'Visually or semantically separates content.',
-		github: 'github.com',
-		live: 'live.com'
-	},
-	{
-		title: 'Tabs',
-		image:
-			'https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-		stack: '.',
-		href: '/docs/primitives/tabs',
-		description:
-			'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-		github: 'github.com',
-		live: 'live.com'
-	},
-	{
-		title: 'Tooltip',
-		image:
-			'https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80',
-		stack: '.',
-		href: '/docs/primitives/tooltip',
-		description:
-			'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
 		github: 'github.com',
 		live: 'live.com'
 	}
