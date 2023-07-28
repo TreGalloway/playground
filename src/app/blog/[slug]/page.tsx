871fcf9a2a2fd152e6b676f38f61084b2d8247fc
Tre <tregallowayofficial@gmail.com>
2023-07-28 09:29:35 -0500

    Adding styling to blog post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { allPosts } from 'contentlayer/generated';
 import { getMDXComponent } from 'next-contentlayer/hooks';
 import { format, parseISO } from 'date-fns';
 import { Mdx } from '@/components/mdx';
-import Image from 'next/image';
+import Balancer from 'react-wrap-balancer';
 
 export const generateStaticParams = async () =>
 	allPosts.map((post: any) => ({ slug: post._raw.flattenedPath }));
@@ -10,6 +10,35 @@ export const generateMetadata = ({ params }: any) => {
 	const post = allPosts.find((post: any) => post._raw.flattenedPath === params.slug);
 	return { title: post?.title, description: post?.description };
 };
+
+function formatDate(date: string) {
+	const currentDate = new Date();
+	const targetDate = new Date(date);
+
+	const yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
+	const monthsAgo = currentDate.getMonth() - targetDate.getMonth();
+	const daysAgo = currentDate.getDate() - targetDate.getDate();
+
+	let formattedDate = '';
+
+	if (yearsAgo > 0) {
+		formattedDate = `${yearsAgo}y ago`;
+	} else if (monthsAgo > 0) {
+		formattedDate = `${monthsAgo}mo ago`;
+	} else if (daysAgo > 0) {
+		formattedDate = `${daysAgo}d ago`;
+	} else {
+		formattedDate = 'Today';
+	}
+
+	const fullDate = targetDate.toLocaleString('en-us', {
+		month: 'long',
+		day: 'numeric',
+		year: 'numeric'
+	});
+
+	return `${fullDate} (${formattedDate})`;
+}
 
 const PostLayout = ({ params }: { params: { slug: string } }) => {
 	const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
@@ -24,8 +53,12 @@ const PostLayout = ({ params }: { params: { slug: string } }) => {
 
 	return (
 		<div>
-			<h1>{post.title}</h1>
-			<p>{format(parseISO(post.date), 'LLLL d, yyyy')}</p>
+			<h1 className="font-bold text-2xl tracking-tighter max-w-[650px]">
+				<Balancer>{post.title}</Balancer>
+			</h1>
+			<div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
+				<p className="text-sm text-neutral-600 dark:text-neutral-400">{formatDate(post.date)}</p>
+			</div>
 			<article>
 				<MDXContent components={{ Mdx }} />
 			</article>
